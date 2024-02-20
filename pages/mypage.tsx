f8d59536ef7bd849c0f09c2ae06ce6d289c96d3d
kervint1 <s1f102302124@iniad.org>
2024-02-21 08:04:19 +0900

    mypage途中経過

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -3,7 +3,16 @@ import React from 'react'
 const mypage = () => {
   return (
     <div>
-      <h1>mypage</h1>
+      <div className='sidebar'>
+        <ul>
+            <li>会員情報</li>
+            <li>募集一覧</li>
+            <li>画面設定</li>
+        </ul>
+      </div>
+      <div className='contents'>
+
+      </div>
     </div>
   )
 }
